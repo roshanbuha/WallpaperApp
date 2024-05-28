305364abb1f13eaa820dccc50cf00422d10741a7
roshanbuha <roshan.srashtasoft.com>
2024-05-28 11:24:57 +0530

    first Commit

--- lib/ui/video_wallpaper.dart
+++ lib/ui/video_wallpaper.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class VideoWallpaper extends StatelessWidget {
   const VideoWallpaper({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
